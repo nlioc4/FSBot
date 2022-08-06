145d9eec3b03c56fd8ade15cb6a45902cc5bacae
Daddy010 <philipp@christoforidis.care>
2022-08-07 00:15:07 +0200

    Trello Async

--- modules/trello.py
+++ modules/trello.py
@@ -1,6 +1,8 @@
-import requests
+import asyncio
 from logging import getLogger
-from requests import HTTPError, Timeout, ConnectionError
+
+import aiohttp
+from aiohttp.abc import HTTPException
 
 key = "5436804843f30f68797083532214f22b"  # API Key for Trello
 token = "a814da22c985da09bca8e64b4c523d16e6a572e07ea17242b183c4f0d9de5724"  # Token they want
@@ -9,12 +11,14 @@ list_id = "62ed98a02d875925d90ff558"  # This is the ID of the list it is adding 
 log = getLogger('fs_bot')
 
 
-def create_card(card_name, card_description):
+async def create_card(card_name, card_description):
     try:
-        url = f"https://api.trello.com/1/cards"
         querystring = {"name": card_name, "desc": card_description, "idList": list_id, "key": key, "token": token}
-        response = requests.request("POST", url, params=querystring)
-        card_id = response.json()["id"]
-        log.info(f"Created suggestion card under ID {card_id}")
-    except (HTTPError, ConnectionError, Timeout) as ex:
-        log.error(f"Error posting card on trello with trace{ex}")
+        url = f"https://api.trello.com/1/cards"
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url, params=querystring):
+                log.info("Created Card.")
+        await asyncio.sleep(4)
+    except HTTPException as ex:
+        log.error(f"Error creating Trello suggestion card with trace {ex}")
+
