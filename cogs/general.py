e3696c1c3e2029afce209284b0b3ce56cb836377
Daddy010 <philipp@christoforidis.care>
2022-08-08 00:34:22 +0200

    added ctx to send, so it actually sends the DM now brgh

--- cogs/general.py
+++ cogs/general.py
@@ -17,14 +17,14 @@ class GeneralCog(commands.Cog, name="GeneralCog"):
     def __init__(self, client):
         self.bot: discord.Bot = client
 
-    @commands.slash_command(name="suggestion", guild_ids=[cfg.general['guild_id']])
+    @commands.slash_command(name="suggestion")
     async def suggestion(self, ctx: discord.ApplicationContext,
                          title: discord.Option(str, "Input your suggestion's title here", required=True),
                          description: discord.Option(str, "Describe your suggestion here", required=True)):
         """Send a suggestion for FSBot to the administration team!"""
 
         await trello.create_card(title, description)
-        await disp.SUGGESTION_ACCEPTED.send_priv(ctx.user.mention)
+        await disp.SUGGESTION_ACCEPTED.send_priv(ctx, ctx.user.mention)
 
 
 def setup(client):
